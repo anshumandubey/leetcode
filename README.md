c195103b7bce22d77bf58a4028428cd5bd3712ce
anshumandubey <anshumandubey1998@gmail.com>
2021-05-23 16:46:30 +0530

    Two Sum and Readme Updated

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# leetcode
-Solutions for the leetcode problems I've solved in Java
+# History of the problems I've solved on leetcode
+* Add two numbers : https://leetcode.com/problems/add-two-numbers/
+* Remove duplicates from sorted array : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+* Reverse integer : https://leetcode.com/problems/reverse-integer/
+* 3 sum : https://leetcode.com/problems/3sum/
+* 3 sum closest : https://leetcode.com/problems/3sum-closest/
+* 2 sum : https://leetcode.com/problems/two-sum/
---
+++ TwoSum.java
@@ -0,0 +1,24 @@
+package com.dubey.leetcode;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    static class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        System.out.println(Arrays.toString(new Solution().twoSum(nums, target)));
+    }
+}
