8985d55103b5bbfb526c0add1b8a0ca543264918
anshumandubey1998 <114756452+anshumandubey1998@users.noreply.github.com>
2022-10-04 19:08:24 +0530

    Create LongestIncreasingSubsequence.java

---
+++ LongestIncreasingSubsequence.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] t = new int[nums.length];
+        int len = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int index = Arrays.binarySearch(t, 0, len, nums[i]);
+            if (index < 0) index = - (index + 1);
+            t[index] = nums[i];
+            if (index == len) len++;
+        }
+        return len;
+    }
+}
