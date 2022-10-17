2507ecd8e2d21c5cf2ae7491ef625b75e9c1a1ef
Abhay Dubey <abhaydubey93@gmail.com>
2022-10-17 20:29:35 +0530

    FirstUniqueChar Update

--- FirstUniqueChar.java
+++ FirstUniqueChar.java
@@ -1,12 +1,19 @@
-public class Solution {
+package com.dubey.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstUniqueChar {
     public int firstUniqChar(String s) {
-            char[] chars = s.toCharArray();
-            Map<Character, Long> frMap = s.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), HashMap::new, Collectors.counting()));
-            for (int i = 0; i < chars.length; i++) {
-                if (frMap.get(chars[i]) == 1) {
-                    return i;
-                }
+        char[] chars = s.toCharArray();
+        Map<Character, Long> frMap = s.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), HashMap::new, Collectors.counting()));
+        for (int i = 0; i < chars.length; i++) {
+            if (frMap.get(chars[i]) == 1) {
+                return i;
             }
-            return -1;
         }
+        return -1;
+    }
 }
